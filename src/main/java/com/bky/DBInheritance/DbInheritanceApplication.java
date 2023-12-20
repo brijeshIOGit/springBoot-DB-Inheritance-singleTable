c5d0ac0f665b3ca172aca716b8ceee33bddd56e4
Brijesh Yadav <briyadav@WKMZT366A8D9.global.publicisgroupe.net>
2023-12-20 18:40:05 +0530

    added bd creds in app.prop

---
+++ src/main/java/com/bky/DBInheritance/DbInheritanceApplication.java
@@ -0,0 +1,13 @@
+package com.bky.DBInheritance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbInheritanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbInheritanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bky/DBInheritance/DbInheritanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bky.DBInheritance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbInheritanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
